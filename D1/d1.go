0bc90e19c958abc429f7af3741e837d94f19ad2c
iamlockon <xdddxyyyxzzz123@gmail.com>
2020-12-01 21:09:21 +0800

    d1

---
+++ D1/d1.go
@@ -0,0 +1,54 @@
+package D1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"path"
+)
+
+var MAGIC_NUMBER = 2020
+
+var nums []int
+var mp map[int]bool
+
+func init() {
+	nums = make([]int, 0)
+	mp = make(map[int]bool)
+}
+
+// Part1
+func Part1() {
+	s, _ :=  os.Getwd()
+	file, err := os.Open(path.Join(s,"D1/d1.txt"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "read file: ", err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		val, _ := strconv.Atoi(scanner.Text())
+		if _, ok := mp[MAGIC_NUMBER-val]; ok {
+			fmt.Println((MAGIC_NUMBER - val) * val)
+		}
+		mp[val] = true
+		nums = append(nums, val)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "stdin:", err)
+	}
+	return
+}
+
+// Part2
+func Part2() {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i+1; j < len(nums); j++ {
+			if _, ok := mp[MAGIC_NUMBER-nums[i]-nums[j]]; ok {
+				fmt.Println(nums[i]*nums[j]*(MAGIC_NUMBER-nums[i]-nums[j]))
+				return
+			}
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/iamlockon/AOC2020/D1"
+)
+
+func main() {
+	D1.Part1()
+	D1.Part2()
+}
